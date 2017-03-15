0b11a571465506b177e5d80ddff001df009f7f93
Victor Stinner <vstinner@redhat.com>
2017-03-15 22:32:17 +0100

    upgrade_pip: add twine

--- bin/upgrade_pip.sh
+++ bin/upgrade_pip.sh
@@ -1,5 +1,5 @@
 MODULES="pip testtools setuptools virtualenv wheel"
 set -e -x
 python2 -m pip install -U $MODULES tox
-python3 -m pip install -U $MODULES vex
+python3 -m pip install -U $MODULES vex twine
 cp /usr/bin/pip2 /usr/bin/pip
