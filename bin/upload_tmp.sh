9dbc6be1122c3905836e7a122511327a4e0df64c
Victor Stinner <victor.stinner@gmail.com>
2017-04-27 12:55:08 +0200

    upload_tmp: use perso SSH key

--- bin/upload_tmp.sh
+++ bin/upload_tmp.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 echo "Upload $# files to ssh.alwaysdata.com:www/tmp"
-scp -v -r "$@" haypo@ssh.alwaysdata.com:www/tmp
+scp -v -i ~/perso -r "$@" haypo@ssh.alwaysdata.com:www/tmp
 for file in "$@"; do
     echo http://www.haypocalc.com/tmp/$(basename "$file")
 done
