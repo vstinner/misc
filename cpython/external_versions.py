bf5688957c3f553e58d1037f3bb2e8af8c4ca9e1
Victor Stinner <victor.stinner@gmail.com>
2017-09-07 18:35:47 -0700

    external_versions.py

---
+++ cpython/external_versions.py
@@ -0,0 +1,51 @@
+# https://haypo-notes.readthedocs.io/cpython.html#embedded-libraries
+import re
+
+def grep(filename, pattern):
+    regex = re.compile(pattern)
+    with open(filename) as fp:
+        for line in fp:
+            match = regex.search(line)
+            if match:
+                return match.group(1)
+    raise ValueError("unable to find %r in %s" % (pattern, filename))
+
+
+def dump_version(name, filename, pattern):
+    try:
+        version = grep(filename, pattern)
+    except FileNotFoundError:
+        return
+    print("%s: %s" % (name, version))
+
+def main():
+    dump_version('ctypes',
+                 'Modules/_ctypes/libffi/configure.ac',
+                 r'AC_INIT\([^,]+, \[([^]]+)\],')
+    dump_version('libffi_osx',
+                 'Modules/_ctypes/libffi_osx/include/fficonfig.h',
+                 r'PACKAGE_VERSION "([^"]+)"')
+    dump_version('libffi_msvc',
+                 'Modules/_ctypes/libffi_msvc/ffi.h',
+                 r'libffi (.*) - Copyright \(c\) ')
+    filename = 'Modules/expat/expat.h'
+    major = grep(filename, r'#define XML_MAJOR_VERSION (.*)')
+    minor = grep(filename, r'#define XML_MINOR_VERSION (.*)')
+    micro = grep(filename, r'#define XML_MICRO_VERSION (.*)')
+    print('expat: %s.%s.%s' % (major, minor, micro))
+    dump_version('zlib',
+                 'Modules/zlib/zlib.h',
+                 r'#define ZLIB_VERSION "(.*)"')
+    dump_version('libmpdec',
+                 'Modules/_decimal/libmpdec/mpdecimal.h',
+                 r'MPD_VERSION "(.*)"')
+    dump_version('[Windows] openssl',
+                 'PCbuild/get_externals.bat',
+                 r'openssl-([0-9].*)')
+    dump_version('[macOS] openssl',
+                 'Mac/BuildScript/build-installer.py',
+                 r'openssl-([0-9][^.]*\.[^.]+\.[^.]+)')
+
+
+if __name__ == "__main__":
+    main()
