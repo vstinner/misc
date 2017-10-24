10504d432ac3481810b171020e1015123fb423a1
Victor Stinner <victor.stinner@gmail.com>
2017-10-24 10:55:32 +0200

    cleanup

--- misc/rename_performance_json.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-import os
-for name in os.listdir():
-    name2 = '%s (%s).%s' % (name[:10], name[24:36], name[37:])
-    print("%s -> %s" % (name, name2))
-    os.rename(name, name2)
