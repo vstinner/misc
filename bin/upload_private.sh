fcae6c2a4327d09349ad42f7c70d7282dc2baf53
Victor Stinner <victor.stinner@gmail.com>
2017-12-15 15:53:19 +0100

    update upload scripts

--- bin/upload_private.sh
+++ bin/upload_private.sh
@@ -3,5 +3,5 @@ set -e
 echo "Upload $# files to ssh-haypo.alwaysdata.net:www/tmp"
 scp -v -r "$@" haypo@ssh-haypo.alwaysdata.net:www/
 for file in "$@"; do
-    echo http://www.haypocalc.com/$(basename "$file")
+    echo http://haypo.alwaysdata.net/$(basename "$file")
 done
--- bin/upload_tmp.sh
+++ bin/upload_tmp.sh
@@ -3,5 +3,5 @@ set -e
 echo "Upload $# files to ssh-haypo.alwaysdata.net:www/tmp"
 scp -v -i ~/perso -r "$@" haypo@ssh-haypo.alwaysdata.net:www/tmp
 for file in "$@"; do
-    echo http://www.haypocalc.com/tmp/$(basename "$file")
+    echo http://haypo.alwaysdata.net/tmp/$(basename "$file")
 done
