c7442d600e12490ed14ea8b94779e5b4642f7669
Victor Stinner <victor.stinner@gmail.com>
2018-01-12 17:58:42 +0100

    all_locales.py

--- python/all_locales.py
+++ python/all_locales.py
@@ -3,6 +3,7 @@ import _locale
 import os
 import subprocess
 import sys
+import time
 
 PY3 = (sys.version_info >= (3,))
 
@@ -20,6 +21,55 @@ else:
     proc = subprocess.Popen(['locale', '-a'], stdout=subprocess.PIPE, universal_newlines=True)
     locales = proc.communicate()[0].splitlines()
 
+def test_locale(loc):
+    global nonascii
+
+    codeset = _locale.nl_langinfo(_locale.CODESET)
+    if loc:
+        prefix = loc
+    else:
+        prefix = _locale.setlocale(locale.LC_CTYPE, None)
+    prefix = '%s/%s' % (prefix, codeset)
+
+    lc = locale.localeconv()
+    for field, value in sorted(lc.items()):
+        if not isinstance(value, str):
+            continue
+        try:
+            test_ascii(value)
+        except UnicodeError:
+            print("%s: localeconv()['%s'] = %s" % (prefix, field, ascii(value)))
+            nonascii += 1
+
+    nerr = 0
+    for err in range(1, 150):
+        msg = os.strerror(err)
+        try:
+            test_ascii(msg)
+        except UnicodeError:
+            print("%s: strerror(%s) = %s" % (prefix, err, ascii(msg)))
+            nonascii += 1
+            nerr += 1
+            if nerr > 3:
+                print("%s: (skip next strerror)" % prefix)
+                break
+
+    nerr = 0
+    fmt = "%A %B %Z"
+    for month in range(1, 13):
+        t = time.localtime(time.mktime((2018, month, 1, 12, 0, 0, 0, 0, 0)))
+        msg = time.strftime(fmt, t)
+        try:
+            test_ascii(msg)
+        except UnicodeError:
+            print("%s: strftime(%r) = %s" % (prefix, fmt, ascii(msg)))
+            nonascii += 1
+            nerr += 1
+            if nerr > 3:
+                print("%s: (skip next strftime)" % prefix)
+                break
+
+
 invalid_locales = 0
 nonascii = 0
 for loc in locales:
@@ -29,36 +79,8 @@ for loc in locales:
         invalid_locales += 1
         continue
 
-    codeset = _locale.nl_langinfo(_locale.CODESET)
-    if loc:
-        prefix = loc
-    else:
-        prefix = _locale.setlocale(locale.LC_CTYPE, None)
-    prefix = '%s/%s' % (prefix, codeset)
-
     try:
-        lc = locale.localeconv()
-        for field, value in sorted(lc.items()):
-            if not isinstance(value, str):
-                continue
-            try:
-                test_ascii(value)
-            except UnicodeError:
-                print("%s: localeconv()['%s'] = %s" % (prefix, field, ascii(value)))
-                nonascii += 1
-
-        nerr = 0
-        for err in range(1, 150):
-            msg = os.strerror(err)
-            try:
-                test_ascii(msg)
-            except UnicodeError:
-                print("%s: strerror(%s) = %s" % (prefix, err, ascii(msg)))
-                nonascii += 1
-                nerr += 1
-                if nerr > 3:
-                    print("%s: (skip next strerror)" % prefix)
-                    break
+        test_locale(loc)
     except Exception as exc:
         raise Exception("Error while testing %s: %s" % (loc, exc))
 
