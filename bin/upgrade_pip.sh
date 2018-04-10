6fcac6db4f7de552c59c22c0f8a8b080430be2f5
Victor Stinner <victor.stinner@gmail.com>
2018-04-10 15:29:46 +0200

    upgrade_pip

--- bin/upgrade_pip.sh
+++ bin/upgrade_pip.sh
@@ -3,3 +3,10 @@ set -e -x
 python2 -m pip install -U $MODULES tox
 python3 -m pip install -U $MODULES vex twine
 cp /usr/bin/pip2 /usr/bin/pip
+
+echo
+echo "XXX On Fedora, pip3 -m pip install -U setuptools breaks ensurepip"
+echo "XXX and so venv. Fedora has a downstream ensurepip/rewheel/ which fails"
+echo "XXX if there are multiple"
+echo "XXX /usr/lib/python3.6/site-packages/setuptools-XXX.dist-info/"
+echo "XXX directories."
