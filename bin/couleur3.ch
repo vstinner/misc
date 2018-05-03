73802900a3dce777afcc0b8728a419e7778d2605
Victor Stinner <vstinner@redhat.com>
2018-05-03 15:46:17 +0200

    scripts

--- bin/couleur3.ch
+++ bin/couleur3.ch
@@ -4,10 +4,11 @@ set -x
 
 # URL list: http://www.broadcast.ch/portal.aspx?pid=564&lang=fr
 URL=http://stream.srg-ssr.ch/m/couleur3/mp3_128
+CACHE=1024
 
 
 while true; do
-    mplayer -prefer-ipv4 -ao pulse -quiet -cache 1024 "$URL" "$@"
+    mplayer -prefer-ipv4 -ao pulse -quiet -cache $CACHE "$URL" "$@"
     echo "--- error! sleep... ---"
     sleep 2
 done
--- python/parse_mailman_mbox.py
+++ python/parse_mailman_mbox.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
-import mailbox
+import collections
 import datetime
 import email.utils
-import collections
+import mailbox
+import statistics
 import sys
 
 days = collections.Counter()
 
 filename = sys.argv[1]
 box = mailbox.mbox(filename)
+start_date = None
 for msg in box.values():
     dates = msg.get_all('Date')
     if not dates or not dates[0]:
@@ -17,20 +19,24 @@ for msg in box.values():
     date = dates[0]
 
     date = email.utils.parsedate_tz(dates[0])
-    dt = datetime.datetime(*date[:6]) + datetime.timedelta(seconds=date[-1])
+    dt = datetime.datetime(*date[:6]) - datetime.timedelta(seconds=date[-1])
+    if start_date is None:
+        start_date = dt.date()
     day = dt.day
     days[day] += 1
 
-dt_month = dt
-for day in range(1, 31):
-    nmsg = days[day]
-    dt = dt_month.date().replace(day=day)
-    print("%s: %s" % (dt, nmsg))
-print()
+if 0:
+    for day in range(1, 32):
+        try:
+            dt = start_date.replace(day=day)
+        except ValueError:
+            continue
+        nmsg = days[day]
+        print("%s: %s messages" % (dt, nmsg))
+    print()
 
+avg = statistics.mean(days.values())
 nmsg, day = max((nmsg, day) for day, nmsg in days.items())
-dt = dt_month.date().replace(day=day)
-print("Maximum: %s: %s" % (dt, nmsg))
-
+dt = start_date.replace(day=day)
 total = sum(days.values())
-print("Total: %s" % total)
+print("Total: %s msg; avg: %.1f msg/day; max: %s msg at %s" % (total, avg, nmsg, dt))
