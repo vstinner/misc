e317bd9e6dc57ac4fe2aff0dfb9291850d02b7ac
Victor Stinner <vstinner@python.org>
2020-04-30 02:35:19 +0200

    upgrade_pip

--- bin/upgrade_pip.sh
+++ bin/upgrade_pip.sh
@@ -1,18 +1,3 @@
-MODULES="pip virtualenv wheel"
-#MODULES="$MODULES setuptools"
 set -e -x
-python2 -m pip install --user -U $MODULES
-python3 -m pip install --user -U $MODULES tox twine
-#cp /usr/bin/pip2 /usr/bin/pip
-# pip is now Python 3
-
-# echo
-# echo "XXX On Fedora, pip3 -m pip install -U setuptools breaks ensurepip"
-# echo "XXX and so venv. Fedora has a downstream ensurepip/rewheel/ which fails"
-# echo "XXX if there are multiple"
-# echo "XXX /usr/lib/python3.6/site-packages/setuptools-XXX.dist-info/"
-# echo "XXX directories."
-# echo
-# echo "Workaround:"
-# echo "sudo python3 -m pip uninstall setuptools"
-# echo "sudo dnf reinstall python3-setuptools"
+python3 -m pip install --user -U pip
+python3 -m pip install --user -U wheel tox twine
