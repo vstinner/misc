aaa59a3c91d9e2036c4a981886895dcc3719c479
Victor Stinner <vstinner@python.org>
2020-12-15 15:47:32 +0100

    remove old scripts

--- bin/disable_evolution.sh
+++
@@ -1,6 +0,0 @@
-set -x
-for filename in /usr/share/glib-2.0/schemas/org.gnome.[Ee]volution*; do
-    echo > $filename
-done
-
-echo "Restore files using: dnf install --reinstall evolution-data-server (or something like that)"
