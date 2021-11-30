681926e257cd2a214955375e5999ea1cfeeb0905
Victor Stinner <vstinner@python.org>
2021-12-01 00:28:50 +0100

    search_pypi_top.py

--- cpython/search_pypi_top.py
+++ cpython/search_pypi_top.py
@@ -1,61 +1,86 @@
 #!/usr/bin/python3 -u
+import os
 import re
 import sys
-import os
-import subprocess
+import tarfile
+import zipfile
+
 
 pypi_dir = "PYPI-TOP-5000"
-if 0:
-    pattern_filename = sys.argv[1]
-    with open(pattern_filename, encoding="utf8") as fp:
-        pattern = fp.read().strip()
-else:
-    pattern = os.fsencode(sys.argv[1])
+IGNORED_FILE_EXTENSIONS = (".so,")
 
-print(pattern)
+
+def decompress_tar(filename, mode):
+    with tarfile.open(filename, mode) as tar:
+        while True:
+            member = tar.next()
+            if member is None:
+                break
+            name = member.name
+            if name.endswith(IGNORED_FILE_EXTENSIONS):
+                continue
+            fp = tar.extractfile(member)
+            if fp is None:
+                continue
+            yield (name, fp)
+
+
+def decompress_zip(filename):
+    with zipfile.ZipFile(filename) as zf:
+        for member in zf.filelist:
+            name = member.filename
+            if name.endswith(IGNORED_FILE_EXTENSIONS):
+                continue
+            fp = zf.open(member)
+            yield (name, fp)
+
+
+def decompress(filename):
+    if filename.endswith((".tar.gz", ".tgz")):
+        yield from decompress_tar(filename, "r:gz")
+    elif filename.endswith(".tar.bz2"):
+        yield from decompress_tar(filename, "r:bz2")
+    elif filename.endswith(".zip"):
+        yield from decompress_zip(filename)
+    else:
+        raise Exception(f"unsupported filename: {filename!r}")
+
 
 def grep(filename, pattern):
     regex = re.compile(pattern)
 
-    if filename.endswith((".tar.gz", ".tar.bz2", ".tgz")):
-        decompress = ["tar", "-xO", "-f", filename]
-    elif filename.endswith((".zip",)):
-        decompress = ["unzip", "-p", filename]
-    else:
-        raise Exception(f"unsupported filename: {filename!r}")
-
-    cmd1 = subprocess.Popen(decompress, stdout=subprocess.PIPE)
-
     found = []
-    for line in cmd1.stdout:
-        if regex.search(line):
-            found.append(line)
-
-    exitcode1 = cmd1.wait()
-    if exitcode1:
-        print(f"Failed to decompress {filename!r}")
-        sys.exit(exitcode1)
+    for name, fp in decompress(filename):
+        for line in fp:
+            if regex.search(line):
+                found.append((name, line))
 
     return found
 
 def main():
-    projects = 0
-    #for filename in os.listdir(pypi_dir):
-    filename = 'PYPI-TOP-5000/scipy-1.7.3.tar.gz'
-    if 1:
-        #filename = os.path.join(pypi_dir, filename)
+    arg = sys.argv[1]
+    if 0:
+        pattern_filename = arg
+        with open(pattern_filename, encoding="utf8") as fp:
+            pattern = fp.read().strip()
+    else:
+        pattern = os.fsencode(arg)
+
+    total = 0
+    for filename in os.listdir(pypi_dir):
+        filename = os.path.join(pypi_dir, filename)
+        print(f"# grep {filename}", file=sys.stderr)
         lines = grep(filename, pattern)
 
         if lines:
-            print(f"=== {filename} ===")
-            for line in lines:
+            for name, line in lines:
                 line = line.decode('utf8', 'replace').strip()
-                print(line)
-            print()
-            projects += 1
-        #break
+                print(f"{filename}: {name}: {line}")
+            total += len(lines)
 
-    print(f"Matching projects: {projects}")
+    if total:
+        print()
+    print(f"Found {total} matching lines")
 
 
 if __name__ == "__main__":
