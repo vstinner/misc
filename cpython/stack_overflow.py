d657d013c465d20fc13f5dc75a9d22ae45e7ade7
Victor Stinner <vstinner@python.org>
2024-06-05 15:08:41 +0200

    stack_overflow.py

--- cpython/stack_overflow.py
+++ cpython/stack_overflow.py
@@ -1,6 +1,17 @@
+"""
+Links:
+
+* https://bugs.python.org/issue46600
+* https://bugs.python.org/issue30866
+* https://vstinner.github.io/contrib-cpython-2017q1.html
+* https://bugs.python.org/issue29227
+* https://bugs.python.org/issue28858
+"""
+
 from __future__ import print_function
 import sys
 import _testcapi
+import subprocess
 
 
 def test_python_iterator(depth):
@@ -51,50 +62,48 @@ def test_python_getitem(depth):
     return (start, end)
 
 
-if __name__ == '__main__':
-    if len(sys.argv) > 3:
-        test = globals()[sys.argv[1]]
-        depth = int(sys.argv[2])
-        step = int(sys.argv[3])
-        sys.setrecursionlimit(2**18)
-        try:
-            while depth < 10**6:
-                sp_start, sp_end = test(depth)
-                stack_size = abs(sp_start - sp_end) // depth
-                print(depth, stack_size)
-                sys.stdout.flush()
+def worker(test_name, depth, step):
+    test = globals()[test_name]
+    sys.setrecursionlimit(2**18)
+    try:
+        while depth < 10**6:
+            try:
+                sp_start, sp_end = test(depth + step)
+            except RecursionError:
+                step //= 2
+                if step < 10:
+                    return
+                continue
+            else:
                 depth += step
-        except RecursionError:
-            sys.exit(1)
-        except:
-            print('ERROR: %r depth=%d' % (sys.exc_info()[1], depth), file=sys.stderr)
-            sys.exit(1)
+                step *= 2
 
-    import subprocess
-    if len(sys.argv) > 1:
-        tests = [sys.argv[1]]
-        globals()[tests[0]]
-    else:
-        tests = [test for test in sorted(globals()) if test.startswith('test_')]
-        tests = ['test_python_call', 'test_python_getitem', 'test_python_iterator']
+            stack_size = abs(sp_start - sp_end)
+            print(depth, stack_size, flush=True)
+    except:
+        print('ERROR: %r depth=%d' % (sys.exc_info()[1], depth), file=sys.stderr)
+        sys.exit(1)
 
+def run_worker(tests):
     total_depth = 0
     total_stack_size = 0
     for test in tests:
-        step = 2**14
+        step = 10
         stack_size = None
-        depth = 0
+        depth = 50
         retry = 0
+        stop = "crash"
         while True:
-            cmd = [sys.executable, __file__, test, str(depth + step), str(step)]
-            process = subprocess.Popen(cmd, stdout=subprocess.PIPE)
-            stdout, stderr = process.communicate(input)
+            cmd = [sys.executable, __file__, test, str(depth), str(step)]
+            process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            stdout, stderr = process.communicate()
             process.wait()
+
             try:
                 line = stdout.splitlines()[-1]
                 new_depth, new_stack_size = line.split()
                 new_depth = int(new_depth)
-                new_stack_size = int(new_stack_size)
+                new_stack_size = int(new_stack_size) / new_depth
             except IndexError:
                 pass
             else:
@@ -103,7 +112,13 @@ if __name__ == '__main__':
                     stack_size = min(stack_size, new_stack_size)
                 else:
                     stack_size = new_stack_size
-            #print(test, depth, step, file=sys.stderr)
+            if process.returncode == 0:
+                stop = "recursion error"
+            if b"RecursionError" in stderr:
+                stop = "recursion error"
+                break
+            if step == 1:
+                break
             if step > 1:
                 step //= 2
             else:
@@ -111,11 +126,30 @@ if __name__ == '__main__':
             if retry > 10:
                 break
 
+            depth += step
+
         if not stack_size:
             stack_size = 0
-        print("%s: %s calls before crash, stack: %s bytes/call" % (test, depth, stack_size))
+        print(f"{test}: {depth:,} calls before {stop}, stack: {stack_size:.1f} bytes/call")
         total_depth += depth
         total_stack_size += stack_size
 
     print()
-    print("=> total: %s calls, %s bytes" % (total_depth, total_stack_size))
+    print(f"=> total: {total_depth:,} calls and {total_stack_size:.1f} bytes per call")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 3:
+        test_name = sys.argv[1]
+        depth = int(sys.argv[2])
+        step = int(sys.argv[3])
+        worker(test_name, depth, step)
+    else:
+        if len(sys.argv) > 1:
+            tests = [sys.argv[1]]
+        else:
+            tests = [test for test in sorted(globals()) if test.startswith('test_')]
+            tests = ['test_python_call', 'test_python_iterator', 'test_python_getitem']
+
+        run_worker(tests)
+
