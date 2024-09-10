ab48a87adbbf28fd4577db74dc89e6eafbaf9d0c
Victor Stinner <vstinner@python.org>
2024-09-10 16:59:13 +0200

    mymake

--- rust/mymake/src/main.rs
+++ rust/mymake/src/main.rs
@@ -1,51 +1,51 @@
 use std::process::{Command, Stdio};
 use std::io::{BufRead, BufReader};
 
-//fn strip(input: &mut String) {
-//    let len = input.trim_end_matches(&['\r', '\n'][..]).len();
-//    input.truncate(len);
-//}
-
 fn main() {
-    let child = Command::new("make")  // FIXME: pass argv[1:] to make
+    let mut child = Command::new("make")  // FIXME: pass argv[1:] to make
         .stdout(Stdio::piped())
         .spawn().unwrap();
-    let stdout = child.stdout.unwrap();
+    let stdout = child.stdout.take().unwrap();
     let reader = BufReader::new(stdout);
-    // FIXME: exitcode
 
-    let mut warnings = Vec::new();
-    let mut errors = Vec::new();
+    // FIXME: measure duration
+    let mut matched = Vec::new();
     for line in reader.lines() {
         let line = line.unwrap();
-        //strip(&mut line);
         println!("{}", line);
 
         if line.starts_with("warning: ") {
-            warnings.push(line);
+            matched.push(("warning", line));
         }
         else if line.starts_with("error: ") {
-            errors.push(line);
+            matched.push(("error", line));
         }
     }
+    let exitcode = child.wait().expect("process complete");
+    let exitcode = exitcode.code().unwrap();
 
-    if warnings.len() > 0 || errors.len() > 0 {
+    if matched.len() > 0 {
         println!();
-        if warnings.len() > 0 {
-            for line in &warnings {
-                println!("WARNING: {}", line);
+        let mut warnings = 0;
+        let mut errors = 0;
+        if matched.len() > 0 {
+            for line in &matched {
+                println!("{}", line.1);
+                match line.0 {
+                    "warning" => warnings += 1,
+                    "error" => errors += 1,
+                    _ => (),
+                }
             }
         }
-        if errors.len() > 0 {
-            for line in &errors {
-                println!("ERROR: {}", line);
-            }
-        }
-        println!("=> Found warnings/errors");
+        println!("=> Found {} warnings and {} errors", warnings, errors);
     }
 
     println!();
-    if warnings.len() > 0 || errors.len() > 0 {
+    if exitcode != 0 {
+        println!("Build FAILED with exit code {}", exitcode);
+    }
+    else if matched.len() > 0 {
         println!("Build OK but with some warnings/errors");
     }
     else {
