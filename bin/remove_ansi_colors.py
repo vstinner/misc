806a06603a46ea167cd3dfea071582d554ee1eef
Victor Stinner <vstinner@python.org>
2026-06-25 17:21:59 +0200

    remove_ansi_colors.py

--- bin/remove_ansi_colors.py
+++ bin/remove_ansi_colors.py
@@ -3,11 +3,16 @@ import sys
 import re
 
 
+# "\033[1m"
 RE_COLOR = re.compile(br'\033\[[0-9]{1,2}m')
+# "\033[1;31m"
+RE_BG_COLOR = re.compile(br'\033\[[0-9]{1,2};[0-9]{1,2}m')
 
 
 def remove_ansi_colors(data):
-    return RE_COLOR.sub(b'', data)
+    data = RE_COLOR.sub(b'', data)
+    data = RE_BG_COLOR.sub(b'', data)
+    return data
 
 
 def main():
